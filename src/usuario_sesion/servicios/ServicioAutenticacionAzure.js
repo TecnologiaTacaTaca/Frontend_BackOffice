a6ef98a6f564dcf50622cde64dea59c15b0be769
Agustin Jaureguiberry <jaureguiberryagustin@gmail.com>
2025-09-26 14:58:16 -0300

    se agrega select_account

--- src/usuario_sesion/servicios/ServicioAutenticacionAzure.js
+++ src/usuario_sesion/servicios/ServicioAutenticacionAzure.js
@@ -1,7 +1,10 @@
 import { useMsal } from "@azure/msal-react";
 
 export const loginAzure = async (instance) => {
-  await instance.loginPopup({ scopes: ["User.Read", "GroupMember.Read.All"] });
+  await instance.loginPopup({
+    scopes: ["User.Read", "GroupMember.Read.All"],
+    prompt: "select_account", // Agregado para forzar selección y completar request
+  });
 };
 
 // Otras funciones adaptadas
